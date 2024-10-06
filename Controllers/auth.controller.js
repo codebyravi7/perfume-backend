b0fa38badd639db5df2007041dbc198000cdb0ab
Ravikant <ravidreamer7@gmail.com>
2024-10-07 00:32:26 +0530

    "commit"

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -7,12 +7,12 @@ export const signup = async (req, res) => {
     const { fullName, username, password, confirmPassword, gender } = req.body;
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ error: "Passwords don't match" });
+      return res.status(400).json({ message: "Passwords don't match" });
     }
     const user = await User.findOne({ username });
 
     if (user) {
-      return res.status(400).json({ error: "Username already exists" });
+      return res.status(400).json({ message: "Username already exists" });
     }
 
     // HASH PASSWORD HERE
@@ -38,6 +38,7 @@ export const signup = async (req, res) => {
       await newUser.save();
 
       res.status(201).json({
+        message: "Signed Up Successfully",
         _id: newUser._id,
         fullName: newUser.fullName,
         username: newUser.username,
@@ -45,17 +46,17 @@ export const signup = async (req, res) => {
         token,
       });
     } else {
-      res.status(400).json({ error: "Invalid user data" });
+      res.status(400).json({ message: "Invalid user data" });
     }
   } catch (error) {
     console.log("Error in signup controller", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 export const login = async (req, res) => {
   try {
-    // console.log("trying to login in backend")
+    console.log("trying to login in backend");
     const { username, password } = req.body;
     const user = await User.findOne({ username });
     const isPasswordCorrect = await bcrypt.compare(
@@ -64,12 +65,13 @@ export const login = async (req, res) => {
     );
 
     if (!user || !isPasswordCorrect) {
-      return res.status(400).json({ error: "Invalid username or password" });
+      return res.status(400).json({ message: "Invalid username or password" });
     }
 
     const token = generateTokenAndSetCookie(user._id, res);
     // console.log("token in login-> ", token);
     res.status(200).json({
+      message: "Logged in successfully",
       _id: user._id,
       fullName: user.fullName,
       username: user.username,
@@ -78,7 +80,7 @@ export const login = async (req, res) => {
     });
   } catch (error) {
     console.log("Error in login controller", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
@@ -95,6 +97,6 @@ export const logout = (req, res) => {
     res.status(200).json({ message: "Logged out successfully", success: true });
   } catch (error) {
     console.log("Error in logout controller", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
